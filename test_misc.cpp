111e09bdf5759f2cfb85b43d41df3c66cbfb8546
Christoph Lehmann <c_lehmann@posteo.de>
2017-06-02 15:25:26 +0200

    played around with non-type template params

---
+++ test_misc.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+template<typename T>
+void f()
+{
+    cout << "type\n";
+}
+
+template<long I> // catches most
+void f()
+{
+    cout << "long " << I << '\n';
+}
+
+template<int const* P> // specialization for any pointer type possible
+void f()
+{
+    cout << "int ptr " << P << '\n';
+}
+
+template<long const* P> // specialization for any pointer type possible
+void f()
+{
+    cout << "long ptr " << P << '\n';
+}
+
+template<void(*F)()>
+void f()
+{
+    cout << "fct " << F << '\n';
+}
+
+template<double(*F)()> // specialization for any function pointer type possible
+void f()
+{
+    cout << "fct (double) " << F << '\n';
+}
+
+// template<auto I> // C++17
+// void f()
+// {
+//     cout << "auto " << I << '\n';
+//     cout << typeid(decltype(I)).name() << '\n';
+// }
+
+// template<bool I>
+// void f()
+// {
+//     cout << "bool " << I << '\n';
+// }
+
+// template<unsigned int I>
+// void f()
+// {
+//     cout << "uint " << I << '\n';
+// }
+
+void g() {}
+double h() {}
+static constexpr int i = 43;
+static constexpr long l = -43;
+
+int main()
+{
+    f<1>();
+    f<false>();
+    f<g>();
+    f<h>();
+    f<&i>();
+    f<&l>();
+    f<double>();
+    decltype(1) a;
+    return 0;
+}
+
