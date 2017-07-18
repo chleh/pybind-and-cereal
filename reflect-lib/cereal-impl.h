f72d627dd1ac0db3d4bc01f1a74c56f0f1d21900
Christoph Lehmann <c_lehmann@posteo.de>
2017-07-18 16:11:11 +0200

    split cereal header

---
+++ reflect-lib/cereal-impl.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <tuple>
+
+namespace detail
+{
+template <class Archive, class Object, class... Pairs, std::size_t... Idcs>
+void save_impl_no_base(Archive& archive,
+                       Object const& obj,
+                       std::tuple<Pairs...>&& fields,
+                       std::index_sequence<Idcs...>)
+{
+    archive(cereal::make_nvp(std::get<Idcs>(fields).first,
+                             obj.*std::get<Idcs>(fields).second)...);
+}
+
+template <class Archive, class Object, class... Pairs, std::size_t... Idcs>
+void save_impl_derived(Archive& archive,
+                       Object const& obj,
+                       std::tuple<Pairs...>&& fields,
+                       std::index_sequence<Idcs...>)
+{
+    archive(cereal::base_class<typename Object::Meta::base>(&obj),
+            cereal::make_nvp(std::get<Idcs>(fields).first,
+                             obj.*std::get<Idcs>(fields).second)...);
+}
+
+template <class Archive, class Object, class... Pairs>
+void save_impl(Archive& archive,
+               Object const& obj,
+               std::tuple<Pairs...>&& fields,
+               std::true_type)
+{
+    using Idcs = std::index_sequence_for<Pairs...>;
+    save_impl_no_base(
+        archive, obj, std::forward<std::tuple<Pairs...>>(fields), Idcs{});
+}
+
+template <class Archive, class Object, class... Pairs>
+void save_impl(Archive& archive,
+               Object const& obj,
+               std::tuple<Pairs...>&& fields,
+               std::false_type)
+{
+    using Idcs = std::index_sequence_for<Pairs...>;
+    save_impl_derived(
+        archive, obj, std::forward<std::tuple<Pairs...>>(fields), Idcs{});
+}
+
+template <class Archive, class Object, class... Pairs, std::size_t... Idcs>
+void load_impl_no_base(Archive& archive,
+                       Object& obj,
+                       std::tuple<Pairs...>&& fields,
+                       std::index_sequence<Idcs...>)
+{
+    archive(cereal::make_nvp(std::get<Idcs>(fields).first,
+                             obj.*std::get<Idcs>(fields).second)...);
+}
+
+template <class Archive, class Object, class... Pairs, std::size_t... Idcs>
+void load_impl_derived(Archive& archive,
+                       Object& obj,
+                       std::tuple<Pairs...>&& fields,
+                       std::index_sequence<Idcs...>)
+{
+    archive(cereal::base_class<typename Object::Meta::base>(&obj),
+            cereal::make_nvp(std::get<Idcs>(fields).first,
+                             obj.*std::get<Idcs>(fields).second)...);
+}
+
+template <class Archive, class Object, class... Pairs>
+void load_impl(Archive& archive,
+               Object& obj,
+               std::tuple<Pairs...>&& fields,
+               std::true_type)
+{
+    using Idcs = std::index_sequence_for<Pairs...>;
+    load_impl_no_base(
+        archive, obj, std::forward<std::tuple<Pairs...>>(fields), Idcs{});
+}
+
+template <class Archive, class Object, class... Pairs>
+void load_impl(Archive& archive,
+               Object& obj,
+               std::tuple<Pairs...>&& fields,
+               std::false_type)
+{
+    using Idcs = std::index_sequence_for<Pairs...>;
+    load_impl_derived(
+        archive, obj, std::forward<std::tuple<Pairs...>>(fields), Idcs{});
+}
+
+}  // namespace detail
