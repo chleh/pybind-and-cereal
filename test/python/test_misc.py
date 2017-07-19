83270e1df87fe8a6cd4ab9ee472278b07ebc6074
Christoph Lehmann <c_lehmann@posteo.de>
2017-07-19 18:41:32 +0200

    added some toy unit test

---
+++ test/python/test_misc.py
@@ -0,0 +1,13 @@
+import unittest
+import types_one as tp
+
+class TestMisc(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_misc(self):
+        print(tp.all_types)
+
+if __name__ == '__main__':
+    unittest.main()
+
