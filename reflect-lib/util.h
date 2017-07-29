5e163cc28f02ac294e382dfb648eaf02ddc451ba
Christoph Lehmann <c_lehmann@posteo.de>
2017-07-29 10:57:43 +0200

    WIP

--- reflect-lib/util.h
+++ reflect-lib/util.h
@@ -1,5 +1,6 @@
 #include <tuple>
 #include <utility>
+#include <vector>
 
 namespace reflect_lib
 {
@@ -19,6 +20,16 @@ void visit_impl(Visitor&& v, std::tuple<Ts...>&& t,
     NoOp{(v(std::forward<Ts>(std::get<Idcs>(t))), 0)...};
 }
 
+template <typename Visitor, typename... Ts, std::size_t... Idcs>
+decltype(auto) apply_impl(Visitor&& v, std::tuple<Ts...>&& t,
+                          std::index_sequence<Idcs...>)
+{
+    using Result = decltype(v(std::get<0>(t)));
+    // TODO use different type!
+    std::vector<Result> result{ v(std::forward<Ts>(std::get<Idcs>(t)))... };
+    return result;
+}
+
 }  // namespace detail
 
 template <typename Visitor, typename... Ts>
@@ -27,6 +38,14 @@ void visit(Visitor&& v, std::tuple<Ts...>&& t)
     using Idcs = std::index_sequence_for<Ts...>;
     detail::visit_impl(std::forward<Visitor>(v),
                        std::forward<std::tuple<Ts...>>(t), Idcs{});
+}
+
+template <typename Visitor, typename... Ts>
+decltype(auto) apply(Visitor&& v, std::tuple<Ts...>&& t)
+{
+    using Idcs = std::index_sequence_for<Ts...>;
+    return detail::apply_impl(std::forward<Visitor>(v),
+                              std::forward<std::tuple<Ts...>>(t), Idcs{});
 }
 
 }  // namespace reflect_lib
--- test/serialization/test_iterate_fields.cpp
+++ test/serialization/test_iterate_fields.cpp
@@ -40,6 +40,40 @@ void output(std::unique_ptr<T> const& obj)
 }
 
 
+class FieldInfoFilter
+{
+    template <typename T>
+    void operator() () {}
+};
+
+template <typename T, typename Key, typename... Keys>
+decltype(auto) getValue(T const& obj, Key&& key, Keys&&... keys)
+{
+    std::string key_s(key);
+    auto accessors = reflect_lib::apply(
+        [&obj, &key_s](auto const& field_info) {
+            if (field_info.first == key_s) {
+                return field_info.first;
+            } else {
+                return (char const*) nullptr;
+            }
+        },
+        T::Meta::fields());
+    for (auto a : accessors) {
+        if (a) return a;
+    }
+
+    return (typename std::decay<decltype(accessors[0])>::type) nullptr;
+}
+
+template <typename T, typename D, typename Key, typename... Keys>
+decltype(auto) getValue(std::unique_ptr<T, D> const& obj,
+                        Key&& key,
+                        Keys&&... keys)
+{
+    return getValue(*obj, std::forward<Key>(key), std::forward<Keys>(keys)...);
+}
+
 int main()
 {
     auto d1 =
@@ -56,6 +90,8 @@ int main()
     d1->b = std::move(d2);
 
     output(d1);
+
+    std::cout << "get value: `" << getValue(d1, "b", "d") << "'\n";
 
     return 0;
 }
