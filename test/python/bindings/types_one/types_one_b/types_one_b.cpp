e44fd9495c46db270775fa2a9ff56e259228eea6
Christoph Lehmann <c_lehmann@posteo.de>
2017-08-03 22:03:58 +0200

    further wrappper for unique ptrs

--- test/python/bindings/types_one/types_one_b/types_one_b.cpp
+++ test/python/bindings/types_one/types_one_b/types_one_b.cpp
@@ -2,6 +2,45 @@
 
 #include "test/types/types_one/types_one_b/types_one_b.h"
 
+namespace types_one
+{
+namespace types_one_b
+{
+
+
+template <typename T>
+class UniquePtrWrapper
+{
+public:
+    UniquePtrWrapper(reflect_lib::smart_ptr<T>& p) : p_(p.new_copied()) {}
+
+    std::unique_ptr<T>&& getR() { return std::move(p_); }
+
+private:
+    std::unique_ptr<T> p_;
+
+public:
+    REFLECT((UniquePtrWrapper<T>), FIELDS(), METHODS(getR))
+};
+
+template <typename T>
+UniquePtrWrapper<T>
+wrap(reflect_lib::smart_ptr<T>& p)
+{
+    std::cout << "use count: " << p.use_count() << '\n';
+    return UniquePtrWrapper<T>(p);
+}
+
+int f(std::unique_ptr<types_one::Base>&& p)
+{
+    std::cout << "p->i " << p->i << '\n';
+    return p->i;
+}
+
+}  // namespace types_one_b
+}  // namespace types_one
+
+
 REFLECT_LIB_PYTHON_MODULE(types_one__types_one_b, module)
 {
     reflect_lib::Module m(module);
@@ -23,4 +62,15 @@ REFLECT_LIB_PYTHON_MODULE(types_one__types_one_b, module)
     // import necessary because of cross-module inheritance
     pybind11::module::import("types_one.types_one_a");
     m.bind<types_one::types_one_b::NoCopyDerived>();
+
+    m.bind<types_one::types_one_b::UniquePtrWrapper<types_one::Base>>();
+    m.module.def("f", [](types_one::types_one_b::UniquePtrWrapper<types_one::Base>& p) {
+        return types_one::types_one_b::f(p.getR());
+    });
+#if 0
+    m.module.def("wrap", [](reflect_lib::smart_ptr<types_one::Base>& p) {
+
+    });
+#endif
+    m.module.def("wrap", &types_one::types_one_b::wrap<types_one::Base>);
 }
