3ad07028cde352f6e130aa7be3dd152a66bfe507
Christoph Lehmann <c_lehmann@posteo.de>
2017-08-18 10:03:29 +0200

    tested custom type casting (regarding None)

--- test/python/bindings/types_one/tst.cpp
+++ test/python/bindings/types_one/tst.cpp
@@ -16,12 +16,43 @@ std::unique_ptr<S> h() { return std::make_unique<S>(); }
 
 int i(S s) { return s.i; };
 
+
+struct P {
+    P() = default;
+    P(std::nullptr_t) {}
+    explicit P(S const& s_) : s(new S(s_)) {}
+
+    std::unique_ptr<S> s;
+};
+
+int from_ptr(P const& p) {
+    if (p.s)
+        return p.s->i;
+    return -1;
+}
+
+
 PYBIND11_MODULE(tst, m)
 {
-    auto c = pybind11::class_<S>(m).def_readwrite("i", &S::i);
+    pybind11::class_<S>(m, "S")
+        .def(pybind11::init<>())
+        .def_readwrite("i", &S::i);
 
     m.def("f", &f);
     // m.def("g", &g); // does not work
     m.def("h", &h);
     m.def("i", &i);
+
+    pybind11::class_<P>(m, "P")
+        .def(pybind11::init<S const&>())
+        .def(pybind11::init<>())
+        .def(pybind11::init<std::nullptr_t>());
+    pybind11::implicitly_convertible<std::nullptr_t, P>();
+
+    // m.def("from_ptr", &from_ptr, pybind11::arg().none(true));
+    m.def("from_ptr", [](pybind11::object const& o) {
+        if (o.is_none()) return from_ptr(P());
+        P const* p = o.cast<P const*>();
+        return from_ptr(*p);
+    });
 }
