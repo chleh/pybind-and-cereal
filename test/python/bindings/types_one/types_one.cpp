69ff8dfeabd5ffa2a404af624a626a70d32e95cb
Christoph Lehmann <c_lehmann@posteo.de>
2017-08-21 19:26:31 +0200

    removed some old code

--- test/python/bindings/types_one/types_one.cpp
+++ test/python/bindings/types_one/types_one.cpp
@@ -20,40 +20,6 @@ struct BaseTrampoline : public Base
     REFLECT_DERIVED((BaseTrampoline), (Base), FIELDS(), METHODS())
 };
 
-template<typename T>
-class UniquePtrReference
-{
-public:
-    UniquePtrReference(reflect_lib::smart_ptr<T> const& p) : p_(p) {}
-
-    reflect_lib::smart_ptr<T> const& get() const { return p_; }
-private:
-    reflect_lib::smart_ptr<T> const& p_;
-
-public:
-    REFLECT((UniquePtrReference<T>), FIELDS(), METHODS())
-};
-/*
-template<typename T>
-class UniquePtrReference
-{
-public:
-    UniquePtrReference(std::unique_ptr<T> const& p) : p_(p) {}
-
-    std::unique_ptr<T> const& get() const { return p_; }
-private:
-    std::unique_ptr<T> const& p_;
-};
-*/
-
-void f(std::unique_ptr<int> const& p) {
-    std::cout << "p: " << p.get() << '\n';
-}
-
-void g(UniquePtrReference<types_one::Base> const& p) {
-    std::cout << "p: " << p.get().get() << '\n';
-}
-
 }  // namespace types_one
 
 REFLECT_LIB_PYTHON_MODULE(types_one, module)
@@ -67,14 +33,4 @@ REFLECT_LIB_PYTHON_MODULE(types_one, module)
     m.bind<types_one::VectorTest>();
 
     m.bind<types_one::NonDefaultConstructible>();
-
-    m.bind<types_one::UniquePtrReference<types_one::Base>>().def(
-        pybind11::init<reflect_lib::smart_ptr<types_one::Base> const&>());
-
-    pybind11::implicitly_convertible<
-        reflect_lib::smart_ptr<types_one::Base>,
-        // types_one::Base,
-        types_one::UniquePtrReference<types_one::Base>>();
-    // m.module.def("f", &f);
-    m.module.def("g", &types_one::g);
 }
--- test/python/bindings/types_one/types_one_b/types_one_b.cpp
+++ test/python/bindings/types_one/types_one_b/types_one_b.cpp
@@ -2,54 +2,6 @@
 
 #include "test/types/types_one/types_one_b/types_one_b.h"
 
-namespace types_one
-{
-namespace types_one_b
-{
-
-template <typename T>
-class RValueReference
-{
-public:
-    static RValueReference<T> new_copied(reflect_lib::smart_ptr<T> const& p)
-    {
-        return RValueReference<T>{p.new_copied()};
-    }
-    static RValueReference<T> new_moved(reflect_lib::smart_ptr<T> const& p)
-    {
-        return RValueReference<T>{p.new_moved()};
-    }
-
-    T&& get() { return std::move(*p_); }
-
-private:
-    RValueReference(T* p) : p_(p) {}
-    std::unique_ptr<T> p_;
-
-public:
-    REFLECT((RValueReference<T>),
-            FIELDS(),
-            METHODS(/*get, getRvalue*/))
-};
-
-template <typename T>
-RValueReference<T>
-copy_to_rvalue_reference(reflect_lib::smart_ptr<T> const& p)
-{
-    return RValueReference<T>::new_copied(p);
-}
-
-template <typename T>
-RValueReference<T>
-move_to_rvalue_reference(reflect_lib::smart_ptr<T> const& p)
-{
-    return RValueReference<T>::new_moved(p);
-}
-
-}  // namespace types_one_b
-}  // namespace types_one
-
-
 REFLECT_LIB_PYTHON_MODULE(types_one__types_one_b, module)
 {
     reflect_lib::Module m(module);
@@ -60,16 +12,4 @@ REFLECT_LIB_PYTHON_MODULE(types_one__types_one_b, module)
     // import necessary because of cross-module inheritance
     pybind11::module::import("types_one.types_one_a");
     m.bind<types_one::types_one_b::NoCopyDerived>();
-
-    m.bind<types_one::types_one_b::RValueReference<
-        std::unique_ptr<types_one::Base>>>();
-    m.bind<types_one::types_one_b::RValueReference<
-        std::unique_ptr<types_one::types_one_a::NoCopy>>>();
-
-    m.module.def(
-        "move_to_rvalue_reference",
-        &types_one::types_one_b::move_to_rvalue_reference<types_one::Base>);
-    m.module.def("move_to_rvalue_reference",
-                 &types_one::types_one_b::move_to_rvalue_reference<
-                     types_one::types_one_a::NoCopy>);
 }
