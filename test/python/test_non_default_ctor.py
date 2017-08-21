2b010afcf1f9776455d12e97536b3a07f9d945e7
Christoph Lehmann <c_lehmann@posteo.de>
2017-08-21 08:07:35 +0200

    added checks to test

--- test/python/test_non_default_ctor.py
+++ test/python/test_non_default_ctor.py
@@ -7,6 +7,8 @@ class TestNonDefaultCtor(unittest.TestCase):
 
     def test_construction(self):
         ndc = types_one.NonDefaultConstructible(5.0, 4)
+        self.assertEqual(5.0, ndc.a)
+        self.assertEqual(4, ndc.b)
 
 if __name__ == '__main__':
     unittest.main()
