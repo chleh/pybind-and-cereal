e69e9f82c575daad20d933d96ac7f64fbd5c1d74
Christoph Lehmann <c_lehmann@posteo.de>
2017-08-23 08:47:11 +0200

    added further pickling test

--- test/python/test_pickle.py
+++ test/python/test_pickle.py
@@ -1,22 +1,45 @@
 import unittest
+import os
 
 import pickle
 
-import types_one
+import types_one.types_one_a
 
-class TestPickle(unittest.TestCase):
+class TestPickleSimple(unittest.TestCase):
     def setUp(self):
+        self.filename = "non_default_constructible.pickle"
         ndc = types_one.NonDefaultConstructible(5.0, 7)
         # help(ndc)
-        print("get state:", ndc.__getstate__())
-        with open("non_default_constructible.pickle", "wb") as fh:
+        with open(self.filename, "wb") as fh:
             pickle.dump(ndc, fh, -1)
 
     def test_unpickle(self):
-        with open("non_default_constructible.pickle", "rb") as fh:
+        with open(self.filename, "rb") as fh:
             ndc = pickle.load(fh)
+        os.unlink(self.filename)
+
         self.assertEqual(5.0, ndc.a)
         self.assertEqual(7, ndc.b)
+
+
+class TestPickleNoncopyableInstance(unittest.TestCase):
+    def setUp(self):
+        self.filename = "nocopy.pickle"
+        nc = types_one.types_one_a.NoCopy()
+        nc.i = 5
+        nc.s = "Hello!"
+
+        with open(self.filename, "wb") as fh:
+            pickle.dump(nc, fh, -1)
+
+    def test_unpickle(self):
+        with open(self.filename, "rb") as fh:
+            nc = pickle.load(fh)
+        os.unlink(self.filename)
+
+        self.assertEqual(5, nc.i)
+        self.assertEqual("Hello!", nc.s)
+
 
 if __name__ == '__main__':
     unittest.main()
