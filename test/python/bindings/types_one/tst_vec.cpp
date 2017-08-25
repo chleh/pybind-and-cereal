9519cf2281e9d3a5c29feb686d73c541e2d4385f
Christoph Lehmann <c_lehmann@posteo.de>
2017-08-25 16:41:30 +0200

    enable assigning lists to vectors

--- test/python/bindings/types_one/tst_vec.cpp
+++ test/python/bindings/types_one/tst_vec.cpp
@@ -13,7 +13,26 @@ PYBIND11_MODULE(tst_vec, m)
 {
     pybind11::class_<S>(m, "S")
         .def(pybind11::init<>())
-        .def_readwrite("v", &S::v)
+        .def_property("v",
+                      [](S& s) { return s.v; },
+                      [](S& s, pybind11::object& v) {
+                          try {
+                              auto v_ = v.cast<std::vector<int>>();
+                              s.v = std::move(v_);
+                              return;
+                          } catch (pybind11::cast_error) {
+                          }
+                          try {
+                              auto l = v.cast<pybind11::list>();
+                              s.v.clear();
+                              s.v.reserve(l.size());
+                              for (auto& e : l)
+                                  s.v.emplace_back(e.cast<int>());
+                              return;
+                          } catch (pybind11::cast_error) {
+                          }
+                          throw pybind11::type_error("wrong argument type");
+                      })
         .def("__getstate__",
              [](S const& s) { return pybind11::make_tuple(s.v); })
         .def("__setstate__", [](S& s, pybind11::tuple& t) {
