38262335f8d81c8be8d1f4fed9c749ef5656dac0
Christoph Lehmann <c_lehmann@posteo.de>
2017-09-13 08:40:30 +0200

    added license comments

--- test/python/bindings/aux.cpp
+++ test/python/bindings/aux.cpp
@@ -1,3 +1,9 @@
+/* Copyright (c) 2017 Christoph Lehmann c_lehmann@posteo.de
+ *
+ * All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+
 #include "reflect-lib/pybind.h"
 
 // empty module used to register auxiliary bindings
--- test/python/bindings/types_one/types_one_a/types_one_a.cpp
+++ test/python/bindings/types_one/types_one_a/types_one_a.cpp
@@ -1,3 +1,9 @@
+/* Copyright (c) 2017 Christoph Lehmann c_lehmann@posteo.de
+ *
+ * All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+
 #include "reflect-lib/pybind.h"
 
 #include "test/types/types_one/types_one_a/types_one_a.h"
